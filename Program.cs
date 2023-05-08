b7e9b44fafc4e9e2b0cbf3f91b7227f912b2f7a9
Alpay Başol <alpay41@gmail.com>
2023-05-08 16:38:41 +0300

    feat: add score

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
+            int score = 0;
+
             Console.WriteLine("Hangman Game");
+
+            Console.WriteLine("Puan: {0}", score);
         }
     }
 }
