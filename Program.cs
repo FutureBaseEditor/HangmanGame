c86692bc1e927d18e5f4c2861b1d5ce48162ee55
Alpay Başol <alpay41@gmail.com>
2023-05-14 22:24:11 +0300

    fixed

--- Program.cs
+++ Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hangman Game");
+
+            char harf[] = {'a','b' };  //diyelim ki böyle bir dizi oluşturduk
+            char selectedLetter = 'a' ;// seçilen harf için bu değişkene atadık
+            for (int i = 0; i < harf[i].length; i++)
+            {
+                if (selectedLetter == harf[i])Console.Write(" "); 
+            // seçilen harf x[i] harf dizisindeki harflerden i index numarasına sahip harfe eşit ise;
+            }
+
+
+
         }
     }
 }
