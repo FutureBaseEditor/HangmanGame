16921d7f70ffd619af006e71545686e6e762face
gulsumAYDOGDUERDİM <gulsumaydogduerdim@gmail.com>
2023-05-20 19:13:07 +0300

    feat:add the letter notation

--- Program.cs
+++ Program.cs
@@ -24,6 +24,60 @@
             Console.WriteLine();
             Console.Clear();
 
+            Random rastgele = new Random();
+            string harf;
+            Console.WriteLine("1.hayvanlar\n2.ulkeler\n3.meslekler\n4.yemekler\n5.sporlar");
+            Console.Write("Hangi kategoriden kelime seçmek istersiniz?");
+            int kategori = int.Parse(Console.ReadLine());
+
+            string[] secim;
+            Console.Write("Bir harf tahmini yapın: ");
+            harf = Console.ReadLine().ToUpper();
+
+            int harfTahminSayisi = 0;
+            switch (kategori)
+            {
+                case 1:
+                    secim = hayvanlar;
+                    break;
+                case 2:
+                    secim = ulkeler;
+                    break;
+                case 3:
+                    secim = meslekler;
+                    break;
+                case 4:
+                    secim = yemekler;
+                    break;
+                case 5:
+                    secim = sporlar;
+                    break;
+
+                default:
+                    Console.WriteLine("Geçersiz seçim!");
+                    return;
+            }
+
+            string kelime = secim[rastgele.Next(secim.Length)].ToUpper();
+
+            string[] tahminDizisi = new string[kelime.Length];
+            for (int i = 0; i < tahminDizisi.Length; i++)
+            {
+                tahminDizisi[i] = "_";
+            }
+            //Console.WriteLine("Kelime: " + string.Join(" ", tahminDizisi));
+
+            for (int i = 0; i < kelime.Length; i++)
+            {
+                if (kelime[i] == harf[0])
+                {
+                    tahminDizisi[i] = harf;
+                }
+
+            }
+            Console.WriteLine("Kelime: " + string.Join(" ", tahminDizisi));
+            harfTahminSayisi++;
+
         }
         
     }
