037764f398f5eea6b15d645c6b0475bd8a3eaf94
ElifSaatcioglu <e.saatcioglu1@gmail.com>
2023-05-20 19:53:19 +0300

    feat:letters-list-added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] hayvanlar = { "Kedi", "Köpek", "At", "Tavşan", "Leylek", "Deve", "Yarasa", "Sincap", "Sinek", "Karınca" };
+            string[] ulkeler = { "Türkiye", "İtalya", "Kanada", "Almanya", "Japonya", "İngiltere", "Rusya", "Fransa", "Meksika", "Brezilya" };
+            string[] meslekler = { "Öğretmen", "Doktor", "Mühendis", "Hemşire", "Polis", "Avukat", "İnşaat işçisi", "Dişçi", "Gazeteci", "Bankacı" };
+            string[] yemekler = { "Makarna", "Kebap", "Pilav", "Pizza", "Köfte", "Tavuk", "Balık", "Mantı", "Börek", "Salata" };
+            string[] sporlar = { "Futbol", "Basketbol", "Voleybol", "Tenis", "Yüzme", "Atletizm", "Boks", "Jimnastik", "Hentbol", "Ragbi" };
+            string[] kategoriler = { "Hayvanlar", "Ülkeler", "Meslekler", "Yemekler", "Sporlar" };
+
+            string title = "Adam Asmaca";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(title);
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Harf Giriniz: ");
+            string harfGirdisi = Console.ReadLine();
+            Console.ResetColor();
+
+            Console.WriteLine();
+            Console.Clear();
+
+            char[] harfler = {'A', ' ', 'B', ' ', 'C', ' ', 'D', ' ', 'E', ' ', 'F', ' ', 'G', ' ','Ğ',' ', 'H', ' ', 'I', ' ', 'İ', ' ', 'J', ' ', 'K', ' ', 'L', ' ', 'M', ' ',
+                'N', ' ','O', ' ','Ö', ' ','P', ' ','R', ' ', 'S', ' ', 'Ş',' ','T', ' ', 'U', ' ', 'Ü', ' ', 'V', ' ', 'Y', ' ','Z',' '};
+
+            for (int harf = 0; harf < harfler.Length; harf++)
+            {
+                Console.Write("{0}", harfler[harf]);
+                if (harf == harfler.Length / 2)
+                    Console.WriteLine();
+            }
+
+
+
         }
     }
+      
 }
+    
+
+    
