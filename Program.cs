46ecaff6f65e93def4c0fa95ab08ce6642c21a9e
gulsumAYDOGDUERDİM <gulsumaydogduerdim@gmail.com>
2023-05-20 21:14:02 +0300

    fix:show the categories

--- Program.cs
+++ Program.cs
@@ -4,12 +4,13 @@
     {
         static void Main(string[] args)
         {
+            string[] kategoriler = { "Hayvanlar", "Ülkeler", "Meslekler", "Yemekler", "Sporlar" };
             string [] hayvanlar = { "Kedi", "Köpek", "At", "Tavşan", "Leylek", "Deve", "Yarasa", "Sincap", "Sinek", "Karınca" };
             string [] ulkeler = { "Türkiye", "İtalya", "Kanada", "Almanya", "Japonya", "İngiltere", "Rusya", "Fransa", "Meksika", "Brezilya" };
             string [] meslekler = { "Öğretmen", "Doktor", "Mühendis", "Hemşire", "Polis", "Avukat", "İnşaat işçisi", "Dişçi", "Gazeteci", "Bankacı" };
             string [] yemekler = { "Makarna", "Kebap", "Pilav", "Pizza", "Köfte", "Tavuk", "Balık", "Mantı", "Börek", "Salata" };
             string [] sporlar = { "Futbol", "Basketbol", "Voleybol", "Tenis", "Yüzme", "Atletizm", "Boks", "Jimnastik", "Hentbol", "Ragbi" };
-            string [] kategoriler = { "Hayvanlar", "Ülkeler", "Meslekler", "Yemekler", "Sporlar" };
+            
            
             string title = "Adam Asmaca";
             Console.ForegroundColor = ConsoleColor.Yellow;
@@ -18,7 +19,7 @@
 
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.Write("Harf Giriniz: ");
-            string harfGirdisi = Console.ReadLine();
+            //string harfGirdisi = Console.ReadLine();
             Console.ResetColor();
 
             Console.WriteLine();
@@ -26,8 +27,22 @@
 
             Random rastgele = new Random();
             string harf;
-            Console.WriteLine("1.hayvanlar\n2.ulkeler\n3.meslekler\n4.yemekler\n5.sporlar");
-            Console.Write("Hangi kategoriden kelime seçmek istersiniz?");
+
+            for (int i = 0; i < kategoriler.Length; i++)
+            {
+                //Console.WriteLine(kategoriler[i]);
+               
+                if (i%2==1)
+                { 
+                    Console.WriteLine(i+1+"-"+kategoriler[i]+" ");
+                }
+                else
+                    Console.Write(i+1+"-"+kategoriler[i]+" ");
+
+            }
+
+
+            Console.Write("\n Hangi kategoriden kelime seçmek istersiniz?");
             int kategori = int.Parse(Console.ReadLine());
 
             string[] secim;
